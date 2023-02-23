a9ded8789467fb42ba5f3c1d18d6a9638e5466ba
Vinícius H <91447833+GaaraSan01@users.noreply.github.com>
2023-02-22 22:10:57 -0300

    Verificação de erros

--- src/home/App.jsx
+++ src/home/App.jsx
@@ -7,15 +7,30 @@ import { Content, Home } from './style'
 import { Loading } from '../components/loading'
 import {FaFacebook, FaInstagram, FaLinkedin, FaGithub, FaReact} from 'react-icons/fa'
 function App() {
+  //Mostrando dados em tela
   const [api, setApi] = useState()
 
-  useEffect(() => {
+  //Tratando a resposta dos dados
+  const [loading, setLoading] = useState(true)
+  const [isError, setIsError] = useState(false)
+
+  //Buscando dados da API do Github
+  const getApiGithub = () => {
     GetApi()
-    .then(response => {
-      setApi(response.data)
+    .then(({data}) => {
+      setApi(data)
     })
-    .catch(error => console.log(error))
-    
+    .catch(error => {
+      console.log(error)
+      setIsError(true)
+    })
+    .finally(
+      setLoading(false)
+    )
+  }
+
+  useEffect(() => {
+    getApiGithub()
   }, [])
 
   return (
@@ -23,8 +38,11 @@ function App() {
       <Home>
         <StyleGlobal />
         <Content>
-          {api &&  <Admin img={api.avatar_url} name={api.name} bio={api.bio}/>}
-          {!api && <Loading />}
+          {loading && <Loading />}
+          {isError && (
+            <p>Erro ao carregar as informações.</p>
+          )}
+          {!loading && api && <Admin img={api.avatar_url} name={api.name} bio={api.bio}/>}
           <ComponentsLinks link={'https://www.instagram.com/vinih.io'} name={'Instagram'}icon={<FaInstagram />}/>
           <ComponentsLinks link={'https://web.facebook.com/profile.php?id=100065599075086'} name={'Facebook'} icon={<FaFacebook/>}/>
           <ComponentsLinks link={'https://www.linkedin.com/in/vin%C3%ADcius-henrique-machado-598435247'} name={'Linkedin'} icon={<FaLinkedin/>}/>
