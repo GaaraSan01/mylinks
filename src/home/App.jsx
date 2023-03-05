9d1cf9176d8b68b07476b06842e3f5d07777c757
Vinícius H <91447833+GaaraSan01@users.noreply.github.com>
2023-03-05 15:15:51 -0300

    Loading

--- src/home/App.jsx
+++ src/home/App.jsx
@@ -5,7 +5,7 @@ import { Admin } from '../components/admin'
 import { ComponentsLinks } from '../components/links'
 import { Content, Home } from './style'
 import { Loading } from '../components/loading'
-import {FaFacebook, FaInstagram, FaLinkedin, FaGithub, FaReact} from 'react-icons/fa'
+import { FaFacebook, FaInstagram, FaLinkedin, FaGithub, FaReact } from 'react-icons/fa'
 function App() {
   //Mostrando dados em tela
   const [api, setApi] = useState()
@@ -17,16 +17,16 @@ function App() {
   //Buscando dados da API do Github
   const getApiGithub = () => {
     GetApi()
-    .then(({data}) => {
-      setApi(data)
-    })
-    .catch(error => {
-      console.log(error)
-      setIsError(true)
-    })
-    .finally(
-      setLoading(false)
-    )
+      .then(({ data }) => {
+        setApi(data)
+      })
+      .catch(error => {
+        console.log(error)
+        setIsError(true)
+      })
+      .finally(
+        setLoading(false)
+      )
   }
 
   useEffect(() => {
@@ -42,12 +42,16 @@ function App() {
           {isError && (
             <p>Erro ao carregar as informações.</p>
           )}
-          {!loading && api && <Admin img={api.avatar_url} name={api.name} bio={api.bio}/>}
-          <ComponentsLinks link={'https://www.instagram.com/vinih.io'} name={'Instagram'}icon={<FaInstagram />}/>
-          <ComponentsLinks link={'https://web.facebook.com/profile.php?id=100065599075086'} name={'Facebook'} icon={<FaFacebook/>}/>
-          <ComponentsLinks link={'https://www.linkedin.com/in/vin%C3%ADcius-henrique-machado-598435247'} name={'Linkedin'} icon={<FaLinkedin/>}/>
-          <ComponentsLinks link={'https://github.com/GaaraSan01'} name={'Github'} icon={<FaGithub/>}/>
-          <ComponentsLinks link={'https://myportifolio-ten.vercel.app/'} name={'Portifólio'} icon={<FaReact/>}/>
+          {!loading && api && (
+            <>
+              <Admin img={api.avatar_url} name={api.name} bio={api.bio} />
+              <ComponentsLinks link={'https://www.instagram.com/vinih.io'} name={'Instagram'} icon={<FaInstagram />} />
+              <ComponentsLinks link={'https://web.facebook.com/profile.php?id=100065599075086'} name={'Facebook'} icon={<FaFacebook />} />
+              <ComponentsLinks link={'https://www.linkedin.com/in/vin%C3%ADcius-henrique-machado-598435247'} name={'Linkedin'} icon={<FaLinkedin />} />
+              <ComponentsLinks link={'https://github.com/GaaraSan01'} name={'Github'} icon={<FaGithub />} />
+              <ComponentsLinks link={'https://myportifolio-ten.vercel.app/'} name={'Portifólio'} icon={<FaReact />} />
+            </>
+          )}
         </Content>
       </Home>
     </>
